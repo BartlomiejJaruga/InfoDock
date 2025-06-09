abcf912a7e4a684b3921907966d344714ac8b4c0
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-06-09 12:55:31 +0200

    feat: - added app contenerization. Whole app is starting with single docker-compose.yml located in main project folder. (READ.ME needs to be updated)

--- Server/app/config.py
+++ Server/app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/infodock")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:admin123@mongodb:27017/infodock")
     WEATHER_API_KEY = os.getenv("WEATHER_API_KEY", "your-weather-api-key")
     WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
     NBP_API_URL = "https://api.nbp.pl/api"
